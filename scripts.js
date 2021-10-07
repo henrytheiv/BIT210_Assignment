725744965ca82bd8fe616d2d1d79af8cd691c0bf
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-07 16:22:19 +0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function dropDownIconChange() { 
+    $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
+} 
+
+function openLoginForm(){
+    document.body.classList.add("showPopUp");
+  }
+  function closeLoginForm(){
+    document.body.classList.remove("showPopUp");
+  }
+
